c6e1df683f181bbef1bc96048da883db0ce9c6ef
lintangwisesa <lintangwisesa@ymail.com>
2020-07-09 16:45:00 +0700

    variable

---
+++ 0_basic.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");  // always write on new line
+      Console.Write("Tes");               // without enter
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
+
+/*
+Line 1: using System means that we can use classes from the System namespace.
+Line 2: A blank line. C# ignores white space. However, multiple lines makes the code more readable.
+Line 3: namespace is a used to organize your code, and it is a container for classes and other namespaces.
+Line 4: The curly braces {} marks the beginning and the end of a block of code.
+Line 5: class is a container for data and methods, which brings functionality to your program. Every line of code that runs in C# must be inside a class. In our example, we named the class Program.
+Line 7: Another thing that always appear in a C# program, is the Main method. Any code inside its curly brackets {} will be executed. You don't have to understand the keywords before and after Main. You will get to know them bit by bit while reading this tutorial.
+Line 9: Console is a class of the System namespace, which has a WriteLine() method that is used to output/print text. In our example it will output "Hello World!".
+If you omit the using System line, you would have to write System.Console.WriteLine() to print/output text.
+Note: Every C# statement ends with a semicolon ;.
+Note: C# is case-sensitive: "MyClass" and "myclass" has different meaning.
+Note: Unlike Java, the name of the C# file does not have to match the class name, but they often do (for better organization). When saving the file, save it using a proper name and add ".cs" to the end of the filename. To run the example above on your computer, make sure that C# is properly installed: Go to the Get Started Chapter for how to install C#. The output should be:
+*/
---
+++ 1_variables.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 12;             // integer, no decimal
+      // long x = 1000;       // integer with long range -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+      // float y = 98.76F;    // float, decimal number, 6-7 digit di blkg koma, harus diakhiri F
+      // double y = 98.76D;   // float with long range, 15 digit di blkg koma, TIDAK harus diakhiri F
+      char huruf = 'A';       // single character, must be inside single quote ''
+      string teks = "Andi";   // text/phrases/sentences, must be inside single quote ""
+      bool isTall = true;     // boolean true/false
+      const int z = 200;      // constant
+
+      Console.WriteLine(x); Console.WriteLine(y); Console.WriteLine(z);
+      Console.WriteLine(huruf); Console.WriteLine(teks); Console.WriteLine(isTall);  
+    }
+  }
+}
