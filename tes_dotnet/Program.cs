018ca84d665d3e5c0716176586d31d81edb06f5c
lintangwisesa <lintangwisesa@ymail.com>
2020-07-13 11:00:17 +0700

    dotnet

---
+++ tes_dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tes_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
